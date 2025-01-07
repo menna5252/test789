83b5502b70e0a2142d23a190761cdb97e0c8b7d1
menna5252 <menna.route@gmail.com>
2025-01-07 04:42:02 +0200

    jjj

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,8 +8,8 @@ export default function Navbar() {
   return (
   <nav className="navbar navbar-expand-lg bg-body-tertiary   " id="navbar-example">
   <div className="container">
-    <a className="navbar-brand" href="/">Start Framework
-    </a>
+    <Link to ="/" className="navbar-brand">Start Framework
+    </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
